3f0564f3f3cd3770ca08979f223d378b48f8158f
shreyasrama <shreyas.rama1@gmail.com>
2024-06-06 17:19:09 +1200

    Add basic details and links to ENAO and Musicalyst

--- src/lib/NodeDetails.svelte
+++ src/lib/NodeDetails.svelte
@@ -15,4 +15,43 @@
     >
     <p class="px-4 pt-4 text-4xl line">{nodeDetails[0].name}</p>
     <p class="p-4 opacity-50">{nodeDetails[0].group}</p>
+
+    <div class="flex flex-col gap-4 px-4 pt-2">
+        {#if nodeDetails[0].group == "genre"}
+            <p>
+                {nodeDetails[0].name} at 
+                <a 
+                    class="underline decoration-dotted"
+                    href="https://everynoise.com/research.cgi?mode=genre&name={nodeDetails[0].name}"
+                    target="_blank"
+                >
+                    Every Noise At Once
+                </a>
+            </p>
+
+            <p>
+                {nodeDetails[0].name} at 
+                <a 
+                    class="underline decoration-dotted"
+                    href="https://musicalyst.com/genre/{nodeDetails[0].name.replace(/\s+/g, "-")}"
+                    target="_blank"
+                >
+                    Musicalyst
+                </a>
+            </p>
+        {/if}
+
+        {#if nodeDetails[0].group == "artist"}
+            <p>
+                {nodeDetails[0].name} at 
+                <a 
+                    class="underline decoration-dotted"
+                    href="https://everynoise.com/artistprofile.cgi?id={nodeDetails[0].spotifyId}"
+                    target="_blank"
+                >
+                    Every Noise At Once
+                </a>
+            </p>
+        {/if}
+    </div>
 </div>
